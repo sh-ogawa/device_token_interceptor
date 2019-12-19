3e8bb64f1c563c5e19420740fb5fa9c2df28874c
sh-ogawa <syogo0417@gmail.com>
2019-12-19 22:51:53 +0900

    要らないファイル全削除

--- ios/Classes/SwiftDeviceTokenInterceptorPlugin.swift
+++ ios/Classes/SwiftDeviceTokenInterceptorPlugin.swift
@@ -5,16 +5,7 @@ public class SwiftDeviceTokenInterceptorPlugin: NSObject, FlutterPlugin {
     
     public static func register(with registrar: FlutterPluginRegistrar) {
         channel = FlutterMethodChannel(name: "ooga04/device_token_interceptor", binaryMessenger: registrar.messenger())
-        //let instance = SwiftDeviceTokenInterceptorPlugin(registrar: registrar, messenger: registrar.messenger())
-        //registrar.addApplicationDelegate(instance)
     }
-
-//    init(registrar: (NSObjectProtocol & FlutterPluginRegistrar)!, messenger: (NSObjectProtocol & FlutterBinaryMessenger)!) {
-//        super.init()
-//
-//        channel = FlutterMethodChannel(name: "ooga04/device_token_interceptor", binaryMessenger: registrar.messenger())
-//        registrar?.addMethodCallDelegate(self, channel: channel)
-//    }
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         result("this plugin call only ios -> flutter.")
